5a5e4b1b75ccea3ccf02f45465c85a83c5efedc0
rimahour94 <59297120+rimahour94@users.noreply.github.com>
2020-10-29 17:29:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const track=document.querySelector(".carousel_track")
+// const track=document.querySelector(".container_carousel")
+
+// const slidesCont=Array.from(trackl.children)
+// console.log(slidesCont)
+
+
 const slides=Array.from(track.children)
+console.log(slides)
+
+
 const dotsNav=document.querySelector(".contents-li")
 const dots=Array.from(dotsNav.children)
 
 const brgrMenu=document.querySelector(".brgr_line")
 const contents=document.querySelector(".brgr_menu")
-console.log(brgrMenu)
 
 
 const slideWidth=slides[0].getBoundingClientRect().width
 console.log(slideWidth)
-slides[0].style.left="0"+"px"
-slides[1].style.left=slideWidth*1+"px"
-slides[2].style.left=slideWidth*2+'px'
+
+slides.forEach((slide,index)=>{
+  slide.style.left=slideWidth*index+"px"
+})
+
+// slides[0].style.left="0"+"px"
+// slides[1].style.left=slideWidth*1+"px"
+// slides[2].style.left=slideWidth*2+'px'
 console.log(slideWidth)
 
 
@@ -44,12 +57,12 @@ brgrMenu.addEventListener("click",(e)=>{
   console.log(slideWidth)
   if(getHeight===0)
   {
-  contents.style.height="100px"
+  contents.style.height="112px"
   contents.style.transition="all .2s"
   getHeight=contents.getBoundingClientRect().height
   console.log(getHeight)
   }
-  else if(getHeight===100){
+  else if(getHeight===112){
     contents.style.height="0px"
   contents.style.transition="all .2s"
   }
